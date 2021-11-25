1e40a3a669e938017d463c86631368d3e8c64b97
Talles Corradini <tallescorradini@gmail.com>
2021-11-25 20:52:47 -0300

    refactor: extract Content and SideBar components from App

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface Props {
+  genres: Array<{
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }>;
+  selectedGenreId: number;
+  onButtonClicked: (arg1: number) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, onButtonClicked }: Props) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onButtonClicked(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
